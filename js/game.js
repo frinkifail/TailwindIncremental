5531764d1186f0a9493eadf39c4285779fa1ed24
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-03-26 20:24:25 +0700

    ok

--- js/game.js
+++ js/game.js
@@ -1,11 +1,16 @@
+function reload_shop_display_dynamic(item) {
+    document.getElementById(`shop-item__${item}-cost`).textContent = shop[item].cost.toLocaleString()
+    document.getElementById(`shop-item__${item}-amount`).textContent = shop[item].amount.toLocaleString()
+}
+
 function update() {
     data.money += data.money_per_tick
 }
 function reload_displays() {
     document.getElementById('money display child child').textContent = data.money.toLocaleString()
     document.getElementById('money display second child child').textContent = data.money_per_tick.toLocaleString()
-    document.getElementById('shop-item__efficiency-cost').textContent = shop.efficiency.cost.toLocaleString()
-    document.getElementById('shop-item__efficiency-amount').textContent = shop.efficiency.amount.toLocaleString()
+    reload_shop_display_dynamic('efficiency')
+    reload_shop_display_dynamic('generator')
 }
 
 const game_interval = setInterval(update, 1000)
@@ -40,5 +45,27 @@ function buy_shop_item(item) {
             shop.efficiency.cost *= shop.efficiency.growth
             data.money_per_tick += 1;
         }
+    }
+}
+function buy_shop_item_dynamic(item) {
+    try {
+        if (data.money >= shop[item].cost) {
+            data.money -= shop[item].cost
+            shop[item].amount += 1;
+            shop[item].cost *= shop[item].growth
+            switch (shop[item].method) {
+                case 0:
+                    data.money_per_tick += shop[item].per
+                    break;
+                case 1:
+                    data.money_per_tick *= shop[item].per
+                    break;
+                default:
+                    alert("Warning in buy_shop_item_dynamic: Method not cased")
+                    break;
+            }
+        }
+    } catch {
+        alert("Exception in buy_shop_item_dynamic: Unknown")
     }
 }
