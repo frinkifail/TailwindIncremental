144364a5e8e25e3c02a8a44915b21c863c2539e5
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-03-26 20:24:34 +0700

    recommit since i forgot to save

--- js/data.js
+++ js/data.js
@@ -5,10 +5,32 @@ const data = {
 const elements = {
     content: document.getElementById('body_content')
 }
+
+/* Shop Item Methods - All contributes to data.money_per_tick!
+0 = add
+1 = multiply
+2 = minus update time
+*/
 const shop = {
     efficiency: {
         cost: 10,
         growth: 1.225,
-        amount: 0
+        amount: 0,
+        method: 0,
+        per: 1
+    },
+    generator: {
+        cost: 1000,
+        growth: 1.445,
+        amount: 0,
+        method: 1,
+        per: 2
+    },
+    time_wait: {
+        cost: 5000,
+        growth: 1.885,
+        amount: 1,
+        method: 2,
+        per: 0.1
     }
 }
